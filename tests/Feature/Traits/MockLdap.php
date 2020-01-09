95623181dee13f800d187be71cc620a9bb4b2d85
Wun Chiou <wun@utdallas.edu>
2020-01-09 16:10:06 -0600

    Adds User/Profile creation tests

--- tests/Feature/Traits/MockLdap.php
+++ tests/Feature/Traits/MockLdap.php
@@ -3,9 +3,14 @@
 namespace Tests\Feature\Traits;
 
 use App\User;
+use Adldap\AdldapInterface;
+use Adldap\Query\Builder;
+use Adldap\Query\Collection as AdldapCollection;
+use Adldap\Connections\ProviderInterface;
 use Adldap\Laravel\Facades\Adldap;
 use Adldap\Laravel\Facades\Resolver;
 use Adldap\Models\User as LdapUser;
+use Mockery;
 
 trait MockLdap
 {
@@ -21,7 +26,7 @@ trait MockLdap
         $provider = config('ldap_auth.connection');
         $ldap_sync_attributes = config('ldap_sync.attributes');
 
-        return Adldap::getProvider($provider)->make()->user([
+        $ldap_user = Adldap::getProvider($provider)->make()->user([
             'objectguid' => $user->guid,
             $ldap_sync_attributes['name'] => [$user->name], // samaccountname
             $ldap_sync_attributes['display_name'] => [$user->display_name], // displayname
@@ -32,6 +37,10 @@ trait MockLdap
             $ldap_sync_attributes['pea'] => [$user->email], // mail
             $ldap_sync_attributes['title'] => [$user->title], // title
         ]);
+
+        $ldap_user->exists = true;
+
+        return $ldap_user;
     }
 
     /**
@@ -50,5 +59,45 @@ trait MockLdap
             ->shouldReceive('getDatabaseUsernameColumn')->once()->andReturn('name')
             ->shouldReceive('getLdapDiscoveryAttribute')->once()->andReturn('samaccountname')
             ->shouldReceive('authenticate')->once()->andReturn(true);
+    }
+
+    /**
+     * Mocks the LDAP User Search
+     *
+     * @param User|null $user User from which to find LdapUser with the same attributes, or null to return no results
+     * @return void
+     */
+    protected function mockLdapUserSearch($user = null): void
+    {
+        $ldap_user = ($user instanceof User) ? $this->makeLdapUser($user) : null;
+        $ldap_users = new AdldapCollection([$ldap_user]);
+
+        $schema = app(AdldapInterface::class)->getDefaultProvider()->getSchema();
+        $provider = Mockery::mock(ProviderInterface::class);
+
+        $provider
+            ->shouldReceive('search')->andReturnSelf()
+            ->shouldReceive('select')->andReturnSelf()
+            ->shouldReceive('where')->andReturnSelf()
+            ->shouldReceive('orWhereContains')->andReturnSelf()
+            ->shouldReceive('getSchema')->andReturn($schema)
+            ->shouldReceive('get')->andReturn($ldap_users);
+
+        $this->partialMock(AdldapInterface::class, function ($ad_mock) use ($provider) {
+            $ad_mock->shouldReceive('getDefaultProvider')->andReturn($provider);
+        });
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        // fix for the config() helper not resolving in tests using Mockery
+        $config = app('config');
+        parent::tearDown();
+        app()->instance('config', $config);
     }
 }
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Profile;
+use App\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
+use Tests\Feature\Traits\HasJson;
+use Tests\Feature\Traits\MockLdap;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use HasJson;
+    use MockLdap;
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * Tests permissions for creating users.
+     *
+     * @return void
+     */
+    public function testUserPermissions()
+    {
+        $this->seed();
+
+        $site_admin_role = Role::whereName('site_admin')->firstOrFail();
+        $profiles_editor_role = Role::whereName('profiles_editor')->firstOrFail();
+        $school_profiles_editor_role = Role::whereName('school_profiles_editor')->firstOrFail();
+        $department_profiles_editor_role = Role::whereName('department_profiles_editor')->firstOrFail();
+
+        $user = factory(User::class)->create();
+        $other_user = factory(User::class)->create();
+        auth()->loginUsingId($user->id);
+
+        // normal user
+        $this->get(route('users.create'))->assertStatus(403);
+        $this->get(route('users.index'))->assertStatus(403);
+        $this->get(route('users.show', ['user' => $user]))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $other_user]))->assertStatus(403);
+
+        // site_admin
+        $user->attachRole($site_admin_role);
+        Cache::flush();
+        $this->get(route('users.create'))->assertStatus(200);
+        $this->get(route('users.index'))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $user]))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $other_user]))->assertStatus(200);
+        $user->detachRoles();
+
+        // profiles_editor
+        $user->attachRole($profiles_editor_role);
+        Cache::flush();
+        $this->get(route('users.create'))->assertStatus(200);
+        $this->get(route('users.index'))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $user]))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $other_user]))->assertStatus(200);
+        $user->detachRoles();
+
+        // school_profiles_editor
+        $user->attachRole($school_profiles_editor_role);
+        Cache::flush();
+        $this->get(route('users.create'))->assertStatus(200);
+        $this->get(route('users.index'))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $user]))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $other_user]))->assertStatus(200);
+        $user->detachRoles();
+
+        // department_profiles_editor
+        $user->attachRole($department_profiles_editor_role);
+        Cache::flush();
+        $this->get(route('users.create'))->assertStatus(200);
+        $this->get(route('users.index'))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $user]))->assertStatus(200);
+        $this->get(route('users.show', ['user' => $other_user]))->assertStatus(200);
+        $user->detachRoles();
+    }
+
+    /**
+     * Tests creating a user without a profile
+     *
+     * @return void
+     */
+    public function testUserCreation()
+    {
+        $this->seed();
+        $this->loginAsUserEditor();
+
+        // mock an Ldap user to find
+        $user = factory(User::class)->make();
+        $this->mockLdapUserSearch($user);
+
+        $response = $this->post(route('users.store'), [
+            'name' => $user->name,
+            'create_profile' => false,
+        ]);
+
+        $this->assertDatabaseHas('users', Arr::except($user->getAttributes(), ['guid']));
+
+        $response->assertRedirect(route('users.index'));
+    }
+
+    /**
+     * Tests creating a user found not in LDAP
+     *
+     * @return void
+     */
+    public function testUserNotFound()
+    {
+        $this->seed();
+        $this->loginAsUserEditor();
+
+        // mock an Ldap user not found
+        $name = $this->faker->userName;
+        $this->mockLdapUserSearch();
+
+        $response = $this->post(route('users.store'), [
+            'name' => $name,
+            'create_profile' => true,
+        ]);
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseMissing('users', ['name' => $name]);
+        $this->assertEquals(1, User::count()); // only the editor user
+        $this->assertEquals(0, Profile::count());
+    }
+
+    /**
+     * Tests creating a user with a profile
+     *
+     * @return void
+     */
+    public function testUserCreationWithProfile()
+    {
+        $this->seed();
+        $this->loginAsUserEditor();
+
+        // mock an Ldap user to find
+        $user = factory(User::class)->make();
+        $this->mockLdapUserSearch($user);
+
+        $response = $this->followingRedirects()->post(route('users.store'), [
+            'name' => $user->name,
+            'create_profile' => true,
+        ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('users', Arr::except($user->getAttributes(), ['guid']));
+
+        $this->assertDatabaseHas('profiles', [
+            'user_id' => User::where('name', $user->name)->first()->id,
+            'full_name' => $user->display_name,
+            'first_name' => $user->firstname,
+            'last_name' => $user->lastname,
+            'slug' => $user->pea,
+        ]);
+    }
+
+    /**
+     * Create a logged-in User editor.
+     *
+     * @return User
+     */
+    protected function loginAsUserEditor()
+    {
+        $editor = factory(User::class)->create();
+        $editor->attachRole(Role::whereName('site_admin')->firstOrFail());
+        auth()->loginUsingId($editor->id);
+
+        return $editor;
+    }
+
+}
