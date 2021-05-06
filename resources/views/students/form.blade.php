d5880320be40344fb275fd4710f7f3a7f39df979
Wun Chiou <wun@utdallas.edu>
2021-05-06 13:50:34 -0500

    Students: adds new form questions / formatting

--- resources/views/students/form.blade.php
+++ resources/views/students/form.blade.php
@@ -10,6 +10,12 @@
 </div>
 
 <div class="mb-3">
+    {!! Form::label('research_profile[intro]', 'Which area of research are you most interested in, and why?', ['class' => 'form-label']) !!}
+    <small class="form-text text-muted">Please be concise (500 words)</small>
+    {!! Form::textarea('research_profile[interest]', $student->research_profile->interest ?? '', ['class' => 'form-control', 'required']) !!}
+</div>
+
+<div class="mb-3">
     <label for="topics" class="form-label">Select a few research topics that interest you:</label>
     <div class="tags">
         <livewire:tags-modal :model="$student"> @if($editable)<a class="btn btn-primary btn-sm badge" href="#" data-target="#{{ Illuminate\Support\Str::slug($student->getRouteKey()) }}_tags_editor" data-toggle="modal" role="button"><i class="fas fa-edit"></i> Edit</a>@endif
@@ -86,47 +92,72 @@
         ], $student->research_profile->languages ?? [], ['class' => 'form-control', 'multiple', 'size' => 10]); !!}
 </div>
 
-<div class="mb-3">
-    <strong class="mr-5">Are you willing to travel?</strong>
-    <div class="form-check form-check-inline">
-        {!! Form::radio("research_profile[travel]", '1', $student->research_profile->travel === '1', ['id' => "travel_yes", 'class' => 'form-check-input']) !!}
-        {!! Form::label("travel_yes", "Yes", ['class' => 'form-check-label']) !!}
-    </div>
-    <div class="form-check form-check-inline">
-        {!! Form::radio("research_profile[travel]", '0', $student->research_profile->travel === '0', ['id' => "travel_no", 'class' => 'form-check-input']) !!}
-        {!! Form::label("travel_no", "No", ['class' => 'form-check-label']) !!}
-    </div>
-</div>
-
-<div class="mb-3">
-    <strong class="mr-5">Comfortable working with animals?</strong>
-    <div class="form-check form-check-inline">
-        {!! Form::radio("research_profile[animals]", '1', $student->research_profile->animals === '1', ['id' => "animals_yes", 'class' => 'form-check-input']) !!}
-        {!! Form::label("animals_yes", "Yes", ['class' => 'form-check-label']) !!}
-    </div>
-    <div class="form-check form-check-inline">
-        {!! Form::radio("research_profile[animals]", '0', $student->research_profile->animals === '0', ['id' => "animals_no", 'class' => 'form-check-input']) !!}
-        {!! Form::label("animals_no", "No", ['class' => 'form-check-label']) !!}
+<div class="row mb-4">
+    <strong class="col-lg-9">Are you willing to complete your research hours at one of the research centers in Dallas (<a href="https://calliercenter.utdallas.edu/">Callier Center for Communication Disorders</a>, <a href="https://vitallongevity.utdallas.edu/">Center for Vital Longevity</a> or <a href="https://brainhealth.utdallas.edu">Center for Brain Health</a>)?</strong>
+    <div class="col-lg-3">
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[travel]", '1', $student->research_profile->travel === '1', ['id' => "travel_yes", 'class' => 'form-check-input']) !!}
+            {!! Form::label("travel_yes", "Yes", ['class' => 'form-check-label']) !!}
+        </div>
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[travel]", '0', $student->research_profile->travel === '0', ['id' => "travel_no", 'class' => 'form-check-input']) !!}
+            {!! Form::label("travel_no", "No", ['class' => 'form-check-label']) !!}
+        </div>
     </div>
 </div>
 
-<div class="mb-3">
-    <strong class="mr-5">Need research credit?</strong>
-    <div class="form-check form-check-inline">
-        {!! Form::radio("research_profile[credit]", '1', $student->research_profile->credit === '1', ['id' => "credit_yes", 'class' => 'form-check-input']) !!}
-        {!! Form::label("credit_yes", "Yes, I need credit", ['class' => 'form-check-label']) !!}
+<div class="row mb-4">
+    <strong class="col-lg-9">Are you willing to take part in research that will require you to travel regularly to sites in the Dallas area, such as community centers, participants’ homes or area schools?</strong>
+    <div class="col-lg-3">
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[travel_other]", '1', $student->research_profile->travel === '1', ['id' => "travel_other_yes", 'class' => 'form-check-input']) !!}
+            {!! Form::label("travel_other_yes", "Yes", ['class' => 'form-check-label']) !!}
+        </div>
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[travel_other]", '0', $student->research_profile->travel === '0', ['id' => "travel_other_no", 'class' => 'form-check-input']) !!}
+            {!! Form::label("travel_other_no", "No", ['class' => 'form-check-label']) !!}
+        </div>
     </div>
-    <div class="form-check form-check-inline">
-        {!! Form::radio("research_profile[credit]", '0', $student->research_profile->credit === '0', ['id' => "credit_no", 'class' => 'form-check-input']) !!}
-        {!! Form::label("credit_no", "No, I just want to volunteer", ['class' => 'form-check-label']) !!}
+</div>
+
+<div class="row mb-4">
+    <strong class="col-lg-4">Comfortable working with animals?</strong>
+    <div class="col-lg-8">
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[animals]", '1', $student->research_profile->animals === '1', ['id' => "animals_yes", 'class' => 'form-check-input']) !!}
+            {!! Form::label("animals_yes", "Yes", ['class' => 'form-check-label']) !!}
+        </div>
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[animals]", '0', $student->research_profile->animals === '0', ['id' => "animals_no", 'class' => 'form-check-input']) !!}
+            {!! Form::label("animals_no", "No", ['class' => 'form-check-label']) !!}
+        </div>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <strong class="col-lg-4">Need research credit?</strong>
+    <div class="col-lg-8">
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[credit]", '1', $student->research_profile->credit === '1', ['id' => "credit_yes", 'class' => 'form-check-input']) !!}
+            {!! Form::label("credit_yes", "Yes, I need credit", ['class' => 'form-check-label']) !!}
+        </div>
+        <div class="form-check form-check-inline">
+            {!! Form::radio("research_profile[credit]", '0', $student->research_profile->credit === '0', ['id' => "credit_no", 'class' => 'form-check-input']) !!}
+            {!! Form::label("credit_no", "No, I just want to volunteer", ['class' => 'form-check-label']) !!}
+        </div>
     </div>
 </div>
 
 <div class="form-group row mb-3">
-    {!! Form::label('research_profile[graduation_date]', 'Expected graduation date:', ['class' => 'col-lg-3 col-form-label']) !!}
-    <div class="col-lg-3 col-sm-4">
+    {!! Form::label('research_profile[graduation_date]', 'Expected graduation date:', ['class' => 'col-lg-4 col-form-label']) !!}
+    <div class="col-lg-3 col-md-6">
         {!! Form::text('research_profile[graduation_date]', $student->research_profile->graduation_date ?? null, ['class' => 'form-control', 'data-provide' => 'datepicker', 'data-date-min-view-mode' => '1', 'data-date-format' => 'MM yyyy', 'required']) !!}
     </div>
+</div>
+
+<div class="mb-3">
+    {!! Form::label('research_profile[other_info]', 'Please feel free to add any other relevant information that you think may help us in making this decision', ['class' => 'form-label']) !!}
+    {!! Form::textarea('research_profile[other_info]', $student->research_profile->intro ?? '', ['class' => 'form-control']) !!}
 </div>
 
 @if($editable)
