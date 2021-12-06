2b1088c7f8c86a06a3176af39c93d0b2487e28d7
Wun Chiou <wun@utdallas.edu>
2021-12-06 17:40:46 -0600

    Fixed ProfileDataFactory Faker deprecations

--- database/factories/ProfileDataFactory.php
+++ database/factories/ProfileDataFactory.php
@@ -33,12 +33,12 @@ class ProfileDataFactory extends Factory
             'type' => 'information',
             'sort_order' => 1,
             'data' => [
-                'email' => $this->faker->safeEmail,
-                'phone' => $this->faker->phoneNumber,
-                'title' => $this->faker->jobTitle,
+                'email' => $this->faker->safeEmail(),
+                'phone' => $this->faker->phoneNumber(),
+                'title' => $this->faker->jobTitle(),
                 'secondary_title' => '',
                 'tertiary_title' => '',
-                'location' => $this->faker->lastName . ' ' .
+                'location' => $this->faker->lastName() . ' ' .
                     $this->faker->randomElement($building_suffixes) . ' ' .
                     $this->faker->unique()->randomNumber(4),
             ],
