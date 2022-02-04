2528aa56f1a3378fd89aaa0d2c290296d7799496
Betsy Castro <bedka1028@gmail.com>
2022-02-03 18:28:28 -0600

    Correction on file name, class name and signature of the update orcids artisan command

--- app/Console/Commands/UpdateOrcids.php
+++ app/Console/Commands/UpdateOrcids.php
@@ -7,14 +7,14 @@ use App\Profile;
 use App\ProfileData;
 
 
-class UpdateOrchids extends Command
+class UpdateOrcids extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'orchid:update';
+    protected $signature = 'orcid:update';
 
     /**
      * The console command description.
