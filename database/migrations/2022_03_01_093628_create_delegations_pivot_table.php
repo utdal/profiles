b4b6b231c3f5c3493a588ccb9e73e5d7541d5b19
Betsy Castro <bedka1028@gmail.com>
2022-03-01 14:55:05 -0600

    create_delegates_pivote_table migration and adds delegates and delegators relations to User.php

---
+++ database/migrations/2022_03_01_093628_create_delegations_pivot_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDelegationsPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_delegations', function (Blueprint $table) {
+            $table->integer('delegator_user_id')->unsigned();
+            $table->integer('delegate_user_id')->unsigned();
+            $table->dateTime('starting')->useCurrent();
+            $table->dateTime('until')->nullable();
+            $table->boolean('gets_reminders')->default(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+
+            $table->foreign('delegator_user_id')
+                    ->references('id')->on('users');
+            $table->foreign('delegate_user_id')
+                    ->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delegations_pivot');
+    }
+}
