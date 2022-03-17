d0d35044dada256d5c1596f419119a70ddaf0f69
Wun Chiou <wun@utdallas.edu>
2022-03-17 14:59:18 -0500

    Uses fa-sync icon for loading spinner
    To stay consistent with the existing search spinner. (Also, it looks better!)

--- resources/views/livewire/partials/_loading-indicator.blade.php
+++ resources/views/livewire/partials/_loading-indicator.blade.php
@@ -1,5 +1,5 @@
 <tr wire:loading.delay wire:loading.class="d-flex" role="presentation" class="loading-indicator" wire:key="loading-indicator">
     <th scope="row" colspan="9" class="text-center">
-        <h1><i class="fas fa-spinner fa-spin"></i></h1>
+        <h1><i class="fas fa-sync fa-spin"></i></h1>
     </th>
 </tr>
