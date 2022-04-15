a53eaf446191fd96e5fb4631258a8f9f49fca0b6
Betsy Castro <bedka1028@gmail.com>
2022-04-15 16:56:35 -0500

    Email template redesign

--- resources/views/emails/studentdatareceived.blade.php
+++ resources/views/emails/studentdatareceived.blade.php
@@ -1,13 +1,13 @@
 @extends('emails.template')
 
 @section('message')
-	<p>Dear {{ $name }},</p>
+	<p style="font-weight:bold;"> Dear {{ $name }},</p>
 
 	@if (!$delegate)
-		<p>{{ $count }} undergraduate students have expressed interest in working with you for the {{ $semester }} semester.</p>
+		<p><span style="font-weight:bold;">{{ $count }}</span> undergraduate students have expressed interest in working with you for the <span style="font-weight:bold;">{{ $semester }}</span></p>
 
 	@else
-		<p>You have been delegated to review {{ $count }} applications of undergraduate students that have expressed interest in working with {{ $faculty->full_name }} for the {{ $semester }} semester.</p>
+		<p>You have been delegated to review <span style="font-weight:bold;">{{ $count }}</span> applications of undergraduate students that have expressed interest in working with <span style="font-weight:bold;">{{ $faculty->full_name }}</span> for the <span style="font-weight:bold;">{{ $semester }}</span> semester.</p>
 		
 	@endif
 		
--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -7,29 +7,27 @@
 	<title></title>
 </head>
 
+<!-- Using colors from the settings -->
+@php
+		$bg_primary = $settings['primary_color'] ?? '#008542';
+		$bg_secondary = $settings['secondary_color'] ?? '#008542';
+		$bg_tertiary = $settings['tertiary_color'] ?? '#008542';
+@endphp
+
 <body style="font-family:Helvetica,Arial,sans-serif;box-sizing: border-box;">
 
 	<table style="margin:auto;padding:0;max-width:800px;border-collapse: collapse;">
 		<tbody>
 
 			<!-- Header -->
-
-			<tr style="margin:0;padding:0;width:100%;">
-				<td colspan="3" style="background:#E98300;margin:0;padding:20px 15px;width:50%;color:#fff;font-size:24px;font-weight:300;">
-					<a style="float:left;color:#fff;text-decoration:none;" href="https://www.utdallas.edu/">UT Dallas</a>
+			<tr style="margin:0;padding:0;width:100%;background:{{ $bg_primary }};">
+				<td colspan="6" style="text-align:center;margin:0;padding:20px 15px;width:100%;color:#fff;">
+					<a style="color:#fff;text-decoration:none;" title="{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}" href="{{ url('/') }}">
+						<img style="height:70px;" class="profiles-logo" src="{{ $settings['logo'] ?? asset('img/UTDmono_rev.svg') }}" alt="Logo">
+						<h4>{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}</h4>
+					</a>
 				</td>
-				<td colspan="3" style="background:#E98300;margin:0;padding:20px 15px;width:50%;color:#fff;font-size:24px;font-weight:300;">
-					<a style="float:right;color:#fff;text-decoration:none;" href="https://research.utdallas.edu">Research</a>
-				</td>
 			</tr>
-			<tr style="margin:0;padding:0;width:100%;">
-				<td colspan="6" style="background:#008542;margin:0;height:5px;width:100%;"></td>
-			</tr>
-
-			<tr style="margin:0;padding:0;width:100%;">
-				<td colspan="6" style="background:#69BE28;margin:0;height:15px;width:100%;"></td>
-			</tr>
-			
 
 			<!-- Body -->
 
@@ -40,26 +38,29 @@
 
 					@yield('message')
 					
-					<p>This is an automated message from the <a href="{{ url('/') }}" title="link">UT Dallas Profiles</a> web application.</p>
+					<p>This is an automated message from the <a href="{{ url('/') }}" title="{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}">{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}</a> web application.</p>
 
 					<p>Sincerely,</p>
 
-					<p>Office of Research</p>
+					<p>Office of Research and Innovation</p>
 				</td>
 			</tr>
 
 			<!-- Footer -->
 
-			<tr style="color:#fff;background:#E98300;margin:0;padding:0;width:100%;width:100%;font-size:12px;">
-				<td colspan="3" style="width:40%;text-align:center;padding:15px;">
-					<a style="color:#fff;text-decoration:none;" href="https://www.utdallas.edu/">The University of Texas at Dallas</a><br>
-					800 West Campbell Rd, Richardson, TX 75080 | (972) 883-2111
+			<tr style="color:#fff;margin:0;padding:0;width:100%;font-size:12px;background:{{ $bg_primary }};">
+				<td colspan="6" style="width:100%;text-align:center;padding-top:12px">
 				</td>
-				<td colspan="3" style="width:20%;text-align:center;padding:15px;">
-					Questions or comments? Contact:<br>
-					<a style="color:#fff;text-decoration:none;font-weight:bold;" href="mailto:oris@utdallas.edu">
-						oris@utdallas.edu
-					</a>
+			</tr>
+			<tr style="color:#fff;margin:0;padding:0;width:100%;font-size:16px;background:#C1C1BC;">
+				<td colspan="6" style="width:100%;text-align:center;padding:40px 40px;">
+					@if(isset($settings['footer']))
+						{!! $settings['footer'] !!}
+					@else
+						Questions?<br><a href="/faq">Check our FAQ</a> or <a href="mailto:email@example.com?subject=Profiles">contact us.</a><br><br>
+						<a href="https://example.com">Example Link</a><br><br>
+						<a href="https://example.com">Example Institution</a><br>
+					@endif
 				</td>
 			</tr>
 		</tbody>
