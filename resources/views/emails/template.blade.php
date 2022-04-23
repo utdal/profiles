0e609c78fe78aee57cfa40d53780d5cb46a95cda
Wun Chiou <wun@utdallas.edu>
2022-04-22 19:15:13 -0500

    Email template tweaks

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en-US">
 
+@php
+	$bg_primary = $settings['primary_color'] ?? '#008542';
+	$bg_secondary = $settings['secondary_color'] ?? '#008542';
+	$bg_tertiary = $settings['tertiary_color'] ?? '#69BE28';
+@endphp
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title></title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+	<style>
+		<!--[if (gte mso 9)|(IE)]><!-->
+		body { font-family: 'Roboto', 'Segoe UI', Helvetica, Arial, sans-serif !important; }
+		<!--<![endif]-->
+		a[href], a[href]:visited { color: {{ $bg_primary }}; }
+		.footer a[href], .footer a[href]:visited { color: #fff; }
+	</style>
 </head>
-
-<!-- Using colors from the settings -->
-@php
-		$bg_primary = $settings['primary_color'] ?? '#008542';
-		$bg_secondary = $settings['secondary_color'] ?? '#008542';
-		$bg_tertiary = $settings['tertiary_color'] ?? '#008542';
-@endphp
 
 <body style="font-family:Helvetica,Arial,sans-serif;box-sizing: border-box;">
 
@@ -22,9 +29,9 @@
 			<!-- Header -->
 			<tr style="margin:0;padding:0;width:100%;background:{{ $bg_primary }};">
 				<td colspan="6" style="text-align:center;margin:0;padding:20px 15px;width:100%;color:#fff;">
-					<a style="color:#fff;text-decoration:none;" title="{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}" href="{{ url('/') }}">
-						<img style="height:70px;" class="profiles-logo" src="{{ $settings['logo'] ?? asset('img/UTDmono_rev.svg') }}" alt="Logo">
-						<h4>{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}</h4>
+					<a style="display:flex; align-items: center; justify-content: center; color:#fff;text-decoration:none;" title="{{ $settings['site_title'] ?? 'Profiles' }}" href="{{ url('/') }}">
+						<img style="height:70px; margin-right:1rem;" class="profiles-logo" src="{{ $settings['logo'] ?? asset('img/UTDmono_rev.svg') }}" alt="Logo">
+						<span style="font-size:1.25rem; white-space:nowrap;">{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}</span>
 					</a>
 				</td>
 			</tr>
@@ -38,28 +45,25 @@
 
 					@yield('message')
 					
-					<p>This is an automated message from the <a href="{{ url('/') }}" title="{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}">{{ $settings['site_title'] ?? 'UT Dallas Profiles' }}</a> web application.</p>
+					<hr style="margin-top: 4em;">
 
-					<p>Sincerely,</p>
+					<p>This is an automated message from the <a href="{{ url('/') }}" title="{{ $settings['site_title'] ?? 'Profiles' }}">{{ $settings['site_title'] ?? 'Profiles' }}</a> website.</p>
 
-					<p>Office of Research and Innovation</p>
 				</td>
 			</tr>
 
 			<!-- Footer -->
 
-			<tr style="color:#fff;margin:0;padding:0;width:100%;font-size:12px;background:{{ $bg_primary }};">
+			<tr style="color:#fff;margin:0;padding:0;width:100%;font-size:12px;background:{{ $bg_tertiary }};">
 				<td colspan="6" style="width:100%;text-align:center;padding-top:12px">
 				</td>
 			</tr>
-			<tr style="color:#fff;margin:0;padding:0;width:100%;font-size:16px;background:#C1C1BC;">
+			<tr style="color:#fff;margin:0;padding:0;width:100%;font-size:16px;background:#8B8178;" class="footer">
 				<td colspan="6" style="width:100%;text-align:center;padding:40px 40px;">
 					@if(isset($settings['footer']))
 						{!! $settings['footer'] !!}
 					@else
-						Questions?<br><a href="/faq">Check our FAQ</a> or <a href="mailto:email@example.com?subject=Profiles">contact us.</a><br><br>
-						<a href="https://example.com">Example Link</a><br><br>
-						<a href="https://example.com">Example Institution</a><br>
+						Questions? <a href="{{ url('/faq') }}">Check our FAQ</a> or <a href="mailto:{{ config('mail.from.address') }}?subject=Profiles">contact us.</a>
 					@endif
 				</td>
 			</tr>
