5638be31c91fa6228bccd4c92178af6366a85129
Wun Chiou <wun@utdallas.edu>
2022-04-22 19:16:43 -0500

    Email template: spaces instead of tabs

--- resources/views/emails/studentdatareceived.blade.php
+++ resources/views/emails/studentdatareceived.blade.php
@@ -1,16 +1,16 @@
 @extends('emails.template')
 
 @section('message')
-	<p style="font-weight:bold;"> Dear {{ $name }},</p>
+    <p style="font-weight:bold;"> Dear {{ $name }},</p>
 
-	@if (!$delegate)
-		<p><span style="font-weight:bold;">{{ $count }}</span> undergraduate students have expressed interest in working with you for the <span style="font-weight:bold;">{{ $semester }}</span></p>
+    @if (!$delegate)
+        <p><span style="font-weight:bold;">{{ $count }}</span> undergraduate students have expressed interest in working with you for the <span style="font-weight:bold;">{{ $semester }}</span></p>
 
-	@else
-		<p>You have been delegated to review <span style="font-weight:bold;">{{ $count }}</span> applications of undergraduate students that have expressed interest in working with <span style="font-weight:bold;">{{ $faculty->full_name }}</span> for the <span style="font-weight:bold;">{{ $semester }}</span> semester.</p>
+    @else
+        <p>You have been delegated to review <span style="font-weight:bold;">{{ $count }}</span> applications of undergraduate students that have expressed interest in working with <span style="font-weight:bold;">{{ $faculty->full_name }}</span> for the <span style="font-weight:bold;">{{ $semester }}</span> semester.</p>
 
-	@endif
+    @endif
 
-	<p><a href="{{ route('profiles.students', ['profile' => $faculty]) }}"> Review Student Applications</a></p>
+    <p><a href="{{ route('profiles.students', ['profile' => $faculty]) }}"> Review Student Applications</a></p>
 
 @stop
