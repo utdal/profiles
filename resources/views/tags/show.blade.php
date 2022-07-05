f4c7229007ba88f4ae598ab4f791a18d1afe5f40
Wun Chiou <wun@utdallas.edu>
2022-07-05 12:22:26 -0500

    Some DB call performance improvements

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -2,8 +2,7 @@
 $id = $id ?? Illuminate\Support\Str::slug($model->getRouteKey());
 $tags = $model->tags ?? collect();
 $model_class = get_class($model);
-$all_tags = Spatie\Tags\Tag::getWithType($model_class);
-$random_tags = $all_tags->random(min(15, $all_tags->count()))->pluck('name')->all();
+$random_tags = Spatie\Tags\Tag::whereType($model_class)->inRandomOrder()->limit(10)->pluck('name');
 ?>
 
 {{-- Tag List --}}
@@ -19,7 +18,7 @@ $random_tags = $all_tags->random(min(15, $all_tags->count()))->pluck('name')->al
       <div class="modal-body">
         <div class="container-fluid">
           <div class="alert alert-info">
-            <p>Please type your desired tags, e.g. <strong>{{ implode(', ', $random_tags) }}</strong>, and etc.</p>
+            <p>Please type your desired tags, e.g. <strong>{{ $random_tags->implode(', ') }}</strong>, and etc.</p>
             <p>Press the 'enter' key or type a comma (,) after each new tag.</p>
           </div>
           {!! Form::select($id.'_tags[]', $tags->pluck('name','name')->all(), null, ['id' => $id.'_tags[]', 'multiple', 'data-token' => csrf_token(), 'data-model-name' => $id, 'data-url' => route('tags.api.update'), 'data-model-id' => $model->getKey(), 'data-model' => $model_class]) !!}
