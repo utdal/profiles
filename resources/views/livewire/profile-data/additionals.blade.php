7f1faf8341004c9834f7f2b857eabc8e69337fea
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2022-08-04 18:47:05 -0500

    Fixes plural id of h3 element on the additional sub view.

--- resources/views/livewire/profile-data/additionals.blade.php
+++ resources/views/livewire/profile-data/additionals.blade.php
@@ -1,6 +1,6 @@
 @if(!$data->isEmpty() || $editable)
     <div class="card">
-        <h3 id="additional"><i class="fas fa-sticky-note" aria-hidden="true"></i> Additional Information @if($editable)<a class="btn btn-primary btn-sm" href="{{ route('profiles.edit', [$profile->slug, 'additionals']) }}"><i class="fas fa-edit"></i> Edit</a>@endif</h3>
+        <h3 id="additionals"><i class="fas fa-sticky-note" aria-hidden="true"></i> Additional Information @if($editable)<a class="btn btn-primary btn-sm" href="{{ route('profiles.edit', [$profile->slug, 'additionals']) }}"><i class="fas fa-edit"></i> Edit</a>@endif</h3>
         @foreach($data as $additional)
             <div class="entry">
                 <h5><i class="far fa-sticky-note" aria-hidden="true"></i> {{$additional->title}}</h5>
