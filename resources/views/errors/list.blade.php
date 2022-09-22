110917ee948d647b425125228af68a3eb7282aad
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2022-09-22 09:18:37 -0500

    Fixes typo in error message when adding a user/profile

--- resources/views/errors/list.blade.php
+++ resources/views/errors/list.blade.php
@@ -2,7 +2,7 @@
 <div class="row">
 	<div class="alert alert-danger alert-dismissable col-sm-offset-2 col-sm-9" role="alert">
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		<p><strong>The are some errors. Please correct them and try again.</strong></p>
+		<p><strong>There are some errors. Please correct them and try again.</strong></p>
 		@foreach ($errors->all() as $error)
 			{{ $error }}<br>
 		@endforeach
