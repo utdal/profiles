0a80a2b7fb026f8f941d117d5e09f8707397a662
Wun Chiou <wun@utdallas.edu>
2022-10-13 20:23:05 -0500

    AA: pub data as data-attributes
    so we can more easily read them via JS

--- resources/views/livewire/academic-analytics-publications.blade.php
+++ resources/views/livewire/academic-analytics-publications.blade.php
@@ -28,21 +28,21 @@
 
                         <tbody>
                             @foreach ($this->publications as $pub)
-                                <input type="hidden" name="profile_id" value="{{ $profile->id }}">
-                                
-                                <input type="hidden" name="id" value="{{ $pub['id'] }}">
-                                <input type="hidden" name="sort_order" value="{{ $pub['sort_order'] }}">
-
-                                <input type="hidden" name="data['title']" value="{{ $pub['data']['title'] }}">
-                                <input type="hidden" name="data['year']" value="{{ $pub['data']['year'] }}">
-                                <input type="hidden" name="data['url']" value="{{ $pub['data']['url'] }}">
-                                <input type="hidden" name="data['group']" value=" ">
-                                <input type="hidden" name="data['type']" value="{{ $pub['data']['type'] }}">
-                                <input type="hidden" name="data['status']" value="">
                                 <tr>
-                                    <td><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></td>
-                                    <td> {{ $pub['data']['year'] }}</td>
-                                    <td> {{ $pub['data']['title'] }}</td>
+                                    <td>
+                                        <input
+                                            class="form-check-input"
+                                            type="checkbox"
+                                            value=""
+                                            data-title="{{ $pub->title }}"
+                                            data-year="{{ $pub->year }}"
+                                            data-url="{{ $pub->url }}"
+                                            data-type="{{ $pub->type }}"
+                                            data-doi="{{ $pub->doi }}"
+                                        >
+                                    </td>
+                                    <td> {{ $pub->year }}</td>
+                                    <td> {{ $pub->title }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
