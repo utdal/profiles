d675ca25fb4b540614f987ee72aeee028085170f
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2022-12-16 16:59:51 -0600

    Removes full-width class from footer and other html elements present in all pages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,7 +48,7 @@
 	@endif
 	@yield('content')
 	@yield('footer')
-	<div id="footer-container" class="full-width" style="background-image:url('{{asset('/img/60-lines.png')}}');">
+	<div id="footer-container" style="background-image:url('{{asset('/img/60-lines.png')}}');">
 		<footer class="container">
 			@if(isset($settings['footer']))
 				{!! $settings['footer'] !!}
