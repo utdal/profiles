90f0b84d74cbc962ef8a7a704d7504335b14c131
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2023-01-12 16:55:37 -0600

    Corrects use of variable with cached values that exhausted the memory

--- app/Console/Commands/AddDoiToExistingPublications.php
+++ app/Console/Commands/AddDoiToExistingPublications.php
@@ -30,11 +30,9 @@ class AddDoiToExistingPublications extends Command
      */
     public function handle()
     {
-        //ini_set('memory_limit','1500M');
-
         $starting_character = (string)$this->argument('starting_character');
 
-         $profiles = Cache::remember(
+        $profiles = Cache::remember(
             "profiles-starting-with-{$starting_character}",
             15 * 60,
             fn() => $this->profilesMissingDoi($starting_character)
@@ -44,22 +42,21 @@ class AddDoiToExistingPublications extends Command
         $profiles_bar->setFormat('debug');
         $profiles_bar->start();
 
-        $this->lineAndLog("********** {$profiles->count()} Profiles found with publications without DOI ************");
+        $this->lineAndLog("********** {$profiles->count()} Profiles found with publications without DOI ************ \n");
 
         foreach ($profiles as $profile) {
+
             $profile_publications = $profile->data;
 
             $publications_bar = $this->output->createProgressBar(count($profile->data));
-            $publications_bar->setFormat('debug');
+            $publications_bar->setFormat('debug', '\n');
             $publications_bar->start();
-            //unset($profile->data);
-            echo memory_get_usage();
 
-            $aa_publications = $aa_publications ?? $profile->cachedAAPublications();
+            $aa_publications = $profile->cachedAAPublications();
 
             $publications_found_in_url = $publications_found_in_title = $publications_found_in_aa = $doi_not_found_counter = 0;
 
-            //$this->lineAndLog("********** {$profile_publications->count()} Publications found without DOI for {$profile->full_name} ************\n");
+            $this->lineAndLog("********** {$profile_publications->count()} Publications found without DOI for {$profile->full_name} ************\n");
 
             foreach ($profile_publications as $publication) {
 
@@ -67,62 +64,90 @@ class AddDoiToExistingPublications extends Command
 
                 $doi = $aa_title = null;
 
-                while (is_null($doi)) {
-                    if (!empty($publication->url)) { // Search in the URL
-                        //$this->lineAndLog("Searching DOI in URL");
+                    // Search in the URL
+                    if (!empty($publication->url)) {
+                        $this->lineAndLog("Searching for DOI in URL...");
                         $doi = $this->validateDoiRegex($publication->url);
 
-                        $this->addDoi($publication, $doi, 'url', $publications_found_in_url);
+                        $this->verifyAndSaveDoi($publication, $doi, 'url', $publications_found_in_url, $publications_found_in_title, $publications_found_in_aa);
+
                     }
 
-                    if (!empty($publication->title)) { // Search in the title
-                        //$this->lineAndLog("Searching DOI in the title");
+                    // Search in the title
+                    if (is_null($doi) && !empty($publication->title)) {
+                        $this->lineAndLog("Searching for DOI in the title...");
                         $doi = $this->validateDoiRegex(strip_tags(html_entity_decode($publication->title)));
 
-                        $this->addDoi($publication, $doi, 'title', $publications_found_in_title);
+                        $this->verifyAndSaveDoi($publication, $doi, 'title', $publications_found_in_url, $publications_found_in_title, $publications_found_in_aa);
+
                     }
 
                     // Match the AA Publication
-                    $aa_pub_found = $profile->searchPublicationByTitleAndYear($publication->title, $publication->year, $aa_publications);
+                    if (is_null($doi)) {
+                        $this->lineAndLog("Searching for DOI in Academic Analytics...");
 
-                    $doi = $aa_pub_found[0];
-                    $aa_title = $aa_pub_found[1];
+                        $aa_pub_found = $profile->searchPublicationByTitleAndYear($publication->title, $publication->year,  $aa_publications ?? $profile->cachedAAPublications());
 
-                    $this->addDoi($publication, $doi, 'AA', $publications_found_in_aa, $aa_title);
+                        $doi = $aa_pub_found[0];
+                        $aa_title = $aa_pub_found[1];
 
-                    $doi_not_found_counter++;
+                        $this->verifyAndSaveDoi($publication, $doi, 'Academic Analytics', $publications_found_in_url, $publications_found_in_title, $publications_found_in_aa, $aa_title);
+                    }
 
-                    break;
+                    if (is_null($doi)) { $doi_not_found_counter++; }
+
+                    $publications_bar->advance();
                 }
-                $publications_bar->advance();
 
+                $publications_bar->finish();
+
+                $this->lineAndLogResults($publications_found_in_url, $publications_found_in_title, $publications_found_in_aa, $doi_not_found_counter, $profile->full_name);
+
+                $profiles_bar->advance();
 
             }
-            $publications_bar->finish();
-            //$this->lineAndLogResults($publications_found_in_url, $publications_found_in_title, $publications_found_in_aa, $doi_not_found_counter, $profile->full_name);
-
-            $profiles_bar->advance();
-
-        }
 
         $profiles_bar->finish();
 
         return Command::SUCCESS;
     }
 
-    public function addDoi($publication, &$doi, $search_field, &$counter, $aa_title=null) {
+    /**
+     * Verify given doi. If the doi is not null, updates the profile_data record and increment the counter of doi's found.
+     */
+    public function verifyAndSaveDoi($publication, $doi, $search_field, &$publications_found_in_url, &$publications_found_in_title, &$publications_found_in_aa, $aa_title = null)
+    {
+
         if (is_null($doi)) {
-            $this->lineAndLog("DOI not found in {$search_field}.");
+            $this->line("Not Found");
+            $this->log("DOI not found in {$search_field}.");
         }
         else {
+
             $publication->updateData(['doi' => $doi]);
             if (!is_null($aa_title)) {
                 $publication->insertData(['aa_title' => $aa_title]);
+                $this->log("Title {$publication->title} found as {$aa_title} in Academic Analytics Publications\n.");
             }
+
             $publication->save();
             $this->lineAndLog("DOI {$doi} FOUND IN {$search_field} and updated.");
-            $counter++;
+
+            switch ($search_field)
+            {
+                case 'title':
+                    ++$publications_found_in_title;
+                    break;
+                case 'url':
+                    ++$publications_found_in_url;
+                    break;
+                case 'Academic Analytics':
+                    ++$publications_found_in_aa;
+                    break;
+            }
+
         }
+
     }
 
     /**
@@ -164,9 +189,10 @@ class AddDoiToExistingPublications extends Command
      */
     public function lineAndLogResults($publications_found_in_url, $publications_found_in_title, $publications_found_in_aa, $doi_not_found_counter, $profile_full_name)
     {
-        $this->lineAndLog("TOTAL: \n {$publications_found_in_url} DOI found by url and added successfully.");
-        $this->lineAndLog("{$publications_found_in_title} DOI found by title and added successfully.");
-        $this->lineAndLog("{$publications_found_in_aa} DOI found in Academic Analytics and added successfully.");
+        $this->lineAndLog("TOTAL:");
+        if ($publications_found_in_url > 0 ) { $this->lineAndLog("{$publications_found_in_url} DOI found by url and added successfully."); }
+        if ($publications_found_in_title > 0 ) { $this->lineAndLog("{$publications_found_in_title} DOI found by title and added successfully."); }
+        if ($publications_found_in_aa > 0 ) { $this->lineAndLog("{$publications_found_in_aa} DOI found in Academic Analytics and added successfully."); }
         $this->lineAndLog("{$doi_not_found_counter} PUBLICATIONS NOT FOUND.", 'error');
         $this->lineAndLog("***************** End of Report for {$profile_full_name}  ********************");
     }
@@ -177,6 +203,14 @@ class AddDoiToExistingPublications extends Command
     public function lineAndLog(string $message, string $type = 'info'): void
     {
         $this->line($message, $type);
+        Log::$type($message);
+    }
+
+    /**
+     * Output a message to the log file
+     */
+    public function Log(string $message, string $type = 'info'): void
+    {
         Log::$type($message);
     }
 }
