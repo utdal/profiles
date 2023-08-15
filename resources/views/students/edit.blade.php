00b24fba975a6be2ef7274b7877f143220287c0b
Wun Chiou <wun@utdallas.edu>
2023-08-15 15:15:51 -0500

    Students: moves school-selector JS to edit blade

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -40,3 +40,17 @@
 </div>
 
 @stop
+
+@push('scripts')
+<script>
+    document.addEventListener('livewire:load', function () {
+        $("input[type=checkbox][id^=data_school]").on('change', function() {
+            if ($(this).is(':checked')) {
+                Livewire.emit('addTagType', "App\\Student\\"+$(this).val());
+            } else {
+                Livewire.emit('removeTagType', "App\\Student\\"+$(this).val());
+            }
+        })
+    });
+</script>
+@endpush
