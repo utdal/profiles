0c6e4c10496d7db3d2b9a1c403a401e1f38c5992
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2024-03-12 14:44:57 -0500

    Accessibility: adds aria-label to 'Edit' button in profile data cards
    To fix the 'Links with different destinations' issue in the profiles show page.

--- resources/views/livewire/profile-data-cards/additionals.blade.php
+++ resources/views/livewire/profile-data-cards/additionals.blade.php
@@ -1,5 +1,5 @@
 <section id="additionals" class="card">
-    <h3><i class="fas fa-sticky-note" aria-hidden="true"></i> Additional Information @if($editable)<a class="btn btn-primary btn-sm" href="{{ route('profiles.edit', [$profile->slug, 'additionals']) }}"><i class="fas fa-edit"></i> Edit</a>@endif</h3>
+    <h3><i class="fas fa-sticky-note" aria-hidden="true"></i> Additional Information @if($editable)<a class="btn btn-primary btn-sm" href="{{ route('profiles.edit', [$profile->slug, 'additionals']) }}" aria-label="Edit Additional Information"><i class="fas fa-edit"></i> Edit</a>@endif</h3>
     @foreach($data as $additional)
         <div class="entry">
             <h5><i class="far fa-sticky-note" aria-hidden="true"></i> {{$additional->title}}</h5>
