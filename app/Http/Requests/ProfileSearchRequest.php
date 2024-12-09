0b5983d5b9dfc7cc3fd815ace9fd0bdc8e039a0c
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2024-12-09 15:05:23 -0600

    Validate string type for search input

---
+++ app/Http/Requests/ProfileSearchRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileSearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'search' => 'string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'search.string' => 'The search value must be a string',
+        ]; 
+    }
+}
