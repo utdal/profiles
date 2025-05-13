19b008f8c142af1c914dd23d87fa79d45512f51b
Betsy Castro <5490820+betsyecastro@users.noreply.github.com>
2025-05-13 10:26:48 -0500

    Update API request validations

--- app/Http/Requests/ProfilesApiRequest.php
+++ app/Http/Requests/ProfilesApiRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use App\Profile;
 use App\Rules\AllowedProfileDataType;
+use App\School;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -27,14 +28,34 @@ class ProfilesApiRequest extends FormRequest
     public function rules()
     {
         return [
-            'person' => 'sometimes|string',
-            'search' => 'sometimes|string',
-            'search_names' => 'sometimes|string',
-            'info_contains' => 'sometimes|string',
-            'from_school' => 'sometimes|string',
-            'tag' => 'sometimes|string',
+            'person' => ['sometimes', 'string', 'regex:/^[a-zA-Z0-9.]+$/'],
+            'search' => ['sometimes', 'string', 'alpha_num', 'min:3'],
+            'search_names' => ['sometimes', 'string', 'alpha_num', 'min:3'],
+            'info_contains' => ['sometimes', 'string', 'alpha_num', 'min:3'],
+            'from_school' => [
+                'sometimes',
+                'string',
+                'alpha',
+                function ($attribute, $value, $fail) {
+                    $allowed_schools = School::where('short_name', '!=', 'Other')->pluck('short_name')->toArray();
+                    if(!in_array($value, $allowed_schools)){
+                        $fail("Invalid value for $attribute.");
+                    }
+                }
+            ],
+            'tag' => ['sometimes', 'string', 'alpha_num', 'min:3'],
             'public' => 'sometimes|boolean',
-            'with_data' => 'sometimes|boolean',
+            'with_data' => [
+                'sometimes',
+                'boolean',
+                function ($attribute, $value, $fail) {
+                    if ($value) {
+                        if (empty($this->person)) {
+                            $fail("Parameter not allowed.");
+                        }
+                    }
+                }
+            ],
             'raw_data' => 'sometimes|boolean',
             'data_type' => [
                 'sometimes',
