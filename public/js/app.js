4ce426d6f92f0cb74beef9305a95401c8a721ffc
Betsy Castro <betsy.castro@utdallas.edu>
2026-01-16 16:29:20 -0600

    Removes public param from url for API call to populate the profile picker in student form (#181)

--- public/js/app.js
+++ public/js/app.js
@@ -389,7 +389,7 @@ var profiles = function ($, undefined) {
    * @return {void}
    */
   var registerProfilePicker = function registerProfilePicker(selector, api) {
-    if (typeof api === 'undefined') api = this_url + '/api/v1?with_data=1&data_type=information&public=1';
+    if (typeof api === 'undefined') api = this_url + '/api/v1?with_data=1&data_type=information';
     var $select = $(selector);
     if ($select.length === 0) return;
     if ($select.data('school')) {
